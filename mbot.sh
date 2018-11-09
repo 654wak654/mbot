a0b2f88944babd1730a7b1ebccd1c3275f8e5f36
Ozan Eğitmen <ozanegitmen@gmail.com>
2018-11-09 10:21:12 +0300

    Make script indentation consistent

--- mbot.sh
+++ mbot.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 while getopts ":s" opt; do
-  case $opt in
-    s)
-      export isService=true
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
+    case $opt in
+        s)
+            export isService=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
 done
 
 if [ "$isService" = true ]; then
